5b8492b43e7d29debdff7a25ccb93bd090f6963d
Luis Granado <lagranado@arizona.edu>
2023-11-28 09:28:23 -0700

    PDF works
    Initially i was trying to figure out why buttons wouldn't pull up Power points so i tried converting the PP into HTMLs which didn't work. Then i tried PDFs because you can add those into folders without converting. Turns out the script that pulls the slides was a little messed up so neither worked until i found it. Let me know what you guys want to do.

--- open_pdf.js
+++ open_pdf.js
@@ -1,5 +1,5 @@
 function openPDF(pdfName) {
-  var pdfPath = pdfName; // Assuming PDFs are in the same folder as the HTML
+  var pdfPath = pdfName; 
 
   var pdfIframe = document.getElementById('pdfIframe');
   pdfIframe.src = pdfPath;
